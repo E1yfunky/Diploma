7650583571e26ec17c80e113c5c25748dad44f98
Anna Zueva <anna.zueva2001@gmail.com>
2022-11-30 08:47:08 +0300

    hypercube graphs

--- plotting.py
+++ plotting.py
@@ -12,22 +12,22 @@ def main():
 	print("get started")
 	dimension = 2
 	otn = 3
-	df = pd.read_csv(f"./results/Rosenbrock/{dimension}d/1to{otn}/test_data.csv",  delimiter=';')
+	df = pd.read_csv(f"./results/Rosenbrock/{dimension}d/hypercube/not_centered/test_data.csv",  delimiter=';')
 
 	mas = []
 	for nu in np.linspace(0, 3, 13):
-		mas.append(df[df.nu == nu].groupby(['iteration']).agg({'suitability': 'mean'})['suitability'])
+		mas.append(df[df.nu == nu].groupby(['seed']).agg({'target': 'min'})['target'])
 
 	fig = plt.figure()
 	ax = fig.add_subplot()
 
 	ax.boxplot(mas)
 	ax.set_xticklabels(np.linspace(0, 3, 13))
-	ax.set_title(f'{dimension}d suitability of nu, 1:{otn}')
-	ax.set_ylabel('suitability')
+	ax.set_title(f'{dimension}d target of nu, 1:{otn}')
+	ax.set_ylabel('F*')
 	ax.set_xlabel('Nu')
 
-	plt.savefig(f"./results/Rosenbrock/{dimension}d/1to{otn}/suitability_of_nu.png")
+	plt.savefig(f"./results/Rosenbrock/{dimension}d/hypercube/not_centered/target_of_nu.png")
 	plt.show()
 
 
